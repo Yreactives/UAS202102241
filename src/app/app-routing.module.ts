29ee302dc9626f24140d40797c4f93102c7f6c48
Ronny <103196718+Yreactives@users.noreply.github.com>
2023-01-17 11:19:29 +0700

    First Commit UAS
    Committing to finish exam

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
+const routes: Routes = [
+  {path: "", redirectTo : "mahasiswa", pathMatch : 'full'},
+  {path: "mahasiswa", component: MahasiswaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
+import { HttpClientModule } from "@angular/common/http";  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MahasiswaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
